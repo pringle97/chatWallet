1d9d5881c42e2b164a05a579bbe4f547824745a8
gbroadhead <gwbroadhead@gmail.com>
2022-01-05 13:46:24 -0800

    changes

--- models/post.js
+++ models/post.js
@@ -1,5 +1,16 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../db')
+
+class Post extends Model { }
+Post.init({
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, { sequelize, modelName: 'post' })
+
+module.exports = Post
 
 
 
 
-module.exports =   User, Post
