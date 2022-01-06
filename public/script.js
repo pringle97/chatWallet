d0146663194bf21b34a3b496dd8128d5b95fdce2
gbroadhead <gwbroadhead@gmail.com>
2022-01-06 10:03:37 -0800

    revert

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+
 const socket = io();
 const box = document.querySelector('.chatbox')
 const chat = document.querySelector('.chat-form')
@@ -51,8 +52,8 @@ socket.on('message', message => {
 function outputMessage(message) {
   const div = document.createElement('div')
   div.classList.add('message');
-  div.innerHTML =`<p class="meta" style="background-color:darkblue; color:whitesmoke;>${message.username} @ ${message.time}</p>
-      <p class="text" style="background-color:darkblue; color:whitesmoke;>${message.text}</p><br>`;
+  div.innerHTML =`<div style="background-color:darkblue; color:whitesmoke; border: 2px solid black;"><p class="meta" >${message.username} @ ${message.time}</p>
+      <p class="text">${message.text}</p></div><br>`;
       document.querySelector('.chatbox').appendChild(div)
 }
 
