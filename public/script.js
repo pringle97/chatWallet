3bb52611de8e8229b251afa8fd9183ee1ccd6f59
gbroadhead <gwbroadhead@gmail.com>
2022-01-06 10:06:03 -0800

    test

--- public/script.js
+++ public/script.js
@@ -52,8 +52,8 @@ socket.on('message', message => {
 function outputMessage(message) {
   const div = document.createElement('div')
   div.classList.add('message');
-  div.innerHTML =`<div style="background-color:darkblue; color:whitesmoke; border: 2px solid black;"><p class="meta" >${message.username}</p>
-      <p class="text">${message.text}</p></div><br>`;
+  div.innerHTML =`<div style="background-color:darkblue; color:whitesmoke; border: 2px solid black;"><p class="meta" >${message.username}: </p>
+      <p class="text">>${message.text}</p></div><br>`;
       document.querySelector('.chatbox').appendChild(div)
 }
 
