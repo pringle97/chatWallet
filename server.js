40b3b3bd43be5595c666085b17ebc497d599cbc7
gbroadhead <gwbroadhead@gmail.com>
2022-01-06 01:32:17 -0800

    port change

--- server.js
+++ server.js
@@ -124,5 +124,5 @@ async function init() {
 } 
 init() 
 
-const PORT = 3000 || process.env.PORT
+const PORT = 6565 || process.env.PORT
 server.listen(PORT, () => console.log(`Server running on ${PORT}`))
